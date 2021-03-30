8db6659d12dca05a1ff989132b6679154558632c
Alena_Balchugova <BalchugovaAU@yandex.ru>
2021-03-30 06:58:09 +0300

    Initial commit

---
+++ DZ5/5_1.sql
@@ -0,0 +1,21 @@
+-- 2. Таблица users была неудачно спроектирована. Записи created_at и updated_at были заданы 
+-- типом VARCHAR и в них долгое время помещались значения в формате "20.10.2017 8:10". 
+-- Необходимо преобразовать поля к типу DATETIME, сохранив введеные ранее значения.
+
+DESC users;
+
+UPDATE
+  users
+SET
+  created_at = STR_TO_DATE(created_at, '%d.%m.%Y %k:%i'),
+  updated_at = STR_TO_DATE(updated_at, '%d.%m.%Y %k:%i');
+
+ALTER TABLE
+  users
+CHANGE
+  created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE
+  users
+CHANGE
+  updated_at updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
---
+++ DZ5/5_7.sql
@@ -0,0 +1,5 @@
+-- 3. Подсчитайте произведение чисел в столбце id таблицы users
+
+SELECT ROUND(EXP(SUM(LN(id)))) 
+    AS result 
+    FROM users;
